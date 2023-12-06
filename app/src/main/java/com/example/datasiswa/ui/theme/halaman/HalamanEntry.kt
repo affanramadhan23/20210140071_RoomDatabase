981ed57436011c6eae0164391c3b8282aa0ef8b1
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-06 16:14:00 +0700

    Membuat textfield alamat dan telelpon pada FormInputSiswa

--- app/src/main/java/com/example/datasiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/datasiswa/ui/theme/halaman/HalamanEntry.kt
@@ -3,6 +3,7 @@ package com.example.datasiswa.ui.theme.halaman
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -12,6 +13,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.datasiswa.Destinasi.DestinasiNavigasi
 import com.example.datasiswa.R
@@ -63,6 +65,23 @@ fun FormInputSiswa(
             value = detailSiswa.nama,
             onValueChange = {onValueChange(detailSiswa.copy(nama=it))},
             label = { Text(stringResource(id = R.string.nama))},
+            modifier = modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailSiswa.alamat,
+            onValueChange = {onValueChange(detailSiswa.copy(alamat = it))},
+            label = { Text(stringResource(id = R.string.alamat))},
+            modifier = modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailSiswa.telepon,
+            onValueChange = {onValueChange(detailSiswa.copy(telepon = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number ),
+            label = { Text(stringResource(id = R.string.telepon))},
             modifier = modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
