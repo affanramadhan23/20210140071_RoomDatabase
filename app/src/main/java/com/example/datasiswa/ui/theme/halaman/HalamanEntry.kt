7ad3a4bcb321dba8b2945f045ba0f421338bcc72
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-06 16:08:53 +0700

    Menambahkan parameter pada function FormInputSiswa

--- app/src/main/java/com/example/datasiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/datasiswa/ui/theme/halaman/HalamanEntry.kt
@@ -43,7 +43,10 @@ fun EntrySiswaBody(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInputSiswa(
-
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
 ){
 
 }
