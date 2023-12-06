bb66a98ae51c75974dff4862ef0a832fab225ff0
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-06 15:49:04 +0700

    menambahkan variable coroutineScope dan scrollBehavior pada file HalamanEntry.kt

--- app/src/main/java/com/example/datasiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/datasiswa/ui/theme/halaman/HalamanEntry.kt
@@ -1,6 +1,9 @@
 package com.example.datasiswa.ui.theme.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.datasiswa.Destinasi.DestinasiNavigasi
@@ -13,11 +16,13 @@ object DestinasiEntry: DestinasiNavigasi {
     override val titleRes = R.string.entry_siswa
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntrySiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 }
