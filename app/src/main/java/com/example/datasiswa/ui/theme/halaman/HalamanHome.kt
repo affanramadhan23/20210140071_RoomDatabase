ae171b895a28386bcab3354bd54b6d0875fc0621
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-06 16:22:58 +0700

    Membuat file HalamanHome.kt dan Menambahkan object DestinasiHome

---
+++ app/src/main/java/com/example/datasiswa/ui/theme/halaman/HalamanHome.kt
@@ -0,0 +1,9 @@
+package com.example.datasiswa.ui.theme.halaman
+
+import com.example.datasiswa.Destinasi.DestinasiNavigasi
+import com.example.datasiswa.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
