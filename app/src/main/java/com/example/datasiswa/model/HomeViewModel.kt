6f83c189bf4270dc30ff05d7492e0c8a62eb4aee
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-06 14:38:27 +0700

    menambah timeout sebesar 5ms dan menambah data class HomeUiState pada file HomeViewModel.kt

--- app/src/main/java/com/example/datasiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/datasiswa/model/HomeViewModel.kt
@@ -2,7 +2,14 @@ package com.example.datasiswa.model
 
 import androidx.lifecycle.ViewModel
 import com.example.datasiswa.Repositori.RepositoriSiswa
+import com.example.datasiswa.data.Siswa
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa):ViewModel(){
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+    data class HomeUiState(
+        val listSiswa:List<Siswa> = listOf()
+    )
 
 }
