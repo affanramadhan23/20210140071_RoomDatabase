03a7391d6358d7eda01090d3c405cc72ef46129f
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-06 16:25:19 +0700

    Membuat function HomeScreen beserta parameternya dan variable scrollBehavior

--- app/src/main/java/com/example/datasiswa/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/datasiswa/ui/theme/halaman/HalamanHome.kt
@@ -1,9 +1,26 @@
 package com.example.datasiswa.ui.theme.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.datasiswa.Destinasi.DestinasiNavigasi
 import com.example.datasiswa.R
+import com.example.datasiswa.model.HomeViewModel
+import com.example.datasiswa.model.PenyediaViewModel
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 }
