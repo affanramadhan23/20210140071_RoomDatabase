793e2ce7c3b1b17f50d2ea093795b8b586a5e6c2
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-06 16:46:22 +0700

    Menambahkan function SiswaTopAppBar

--- app/src/main/java/com/example/datasiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/datasiswa/navigasi/PengelolaHalaman.kt
@@ -1,11 +1,20 @@
 package com.example.datasiswa.navigasi
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.datasiswa.R
 import com.example.datasiswa.ui.theme.halaman.DestinasiEntry
 import com.example.datasiswa.ui.theme.halaman.DestinasiHome
 import com.example.datasiswa.ui.theme.halaman.EntrySiswaScreen
@@ -14,6 +23,27 @@ import com.example.datasiswa.ui.theme.halaman.HomeScreen
 @Composable
 fun SiswaApp(navController: NavHostController = rememberNavController()){
 
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+){
+    CenterAlignedTopAppBar(title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = stringResource(id = R.string.back))
+            }
+        }
+    )
 }
 
 @Composable
