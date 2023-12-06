9bcb98ca82b2ef1c92710db7bbf24c5b36650f70
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-06 16:10:32 +0700

    Membuat textfield nama pada FormInputSiswa

--- app/src/main/java/com/example/datasiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/datasiswa/ui/theme/halaman/HalamanEntry.kt
@@ -1,10 +1,17 @@
 package com.example.datasiswa.ui.theme.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.datasiswa.Destinasi.DestinasiNavigasi
 import com.example.datasiswa.R
@@ -48,5 +55,17 @@ fun FormInputSiswa(
     onValueChange: (DetailSiswa) -> Unit = {},
     enabled: Boolean = true
 ){
-
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        OutlinedTextField(
+            value = detailSiswa.nama,
+            onValueChange = {onValueChange(detailSiswa.copy(nama=it))},
+            label = { Text(stringResource(id = R.string.nama))},
+            modifier = modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+    }
 }
